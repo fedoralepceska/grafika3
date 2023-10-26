41690037dfaebe07de3857dd880ecdd83cfad93b
AdoCaice <63158828+AdoCaice@users.noreply.github.com>
2023-10-26 17:50:27 +0200

    tabs2

---
+++ resources/js/Components/TabV2.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="tab-content" v-show="title===selectedTitle">
+        <slot/>
+    </div>
+</template>
+<script>
+import {inject} from "vue";
+export default {
+    props: {
+        title: String
+    },
+    setup(){
+        const selectedTitle = inject("selectedTitle")
+
+        return {
+            selectedTitle
+        }
+    }
+}
+
+</script>
---
+++ resources/js/Components/TabsWrapperV2.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="tabs">
+        <ul class="tabs__header">
+            <li v-for="tab in tabs"
+                :key="tab.props.title"
+                :class="{selected: tab.props.title === selectedTitle}"
+                class="font-weight-bold"
+                @click="selectedTitle = tab.props.title"
+            >
+                {{tab.props.title}}
+                <v-icon>{{tab.props.icon}}</v-icon>
+            </li>
+        </ul>
+        <slot/>
+    </div>
+</template>
+<script>
+import {ref, provide} from "vue";
+
+export default {
+
+    setup(props, {slots}){
+        const tabs = ref(slots.default())
+        // probably not needed
+        const tabTitles = ref(slots.default().map((tab)=> tab.props.title))
+        const selectedTitle = ref(tabTitles.value[0])
+
+        provide("selectedTitle", selectedTitle)
+        return{
+            selectedTitle,
+            tabTitles,
+            tabs
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+.tabs{
+
+}
+.tabs__header{
+    justify-content: center;
+    margin: 15px;
+    list-style: none;
+    padding: 0;
+    display: flex;
+}
+
+.tabs__header li {
+    margin: 2px;
+    min-width: 150px;
+    max-width: 248px;
+    text-align: center;
+    padding: 3px 60px;
+    background-color:$light-gray ;
+    cursor: pointer;
+    transition: 0.4s all ease-out;
+    color: white;
+    border: 1px $green solid;
+    border-radius: 5px;
+}
+.tabs__header li.selected{
+    background-color:$green ;
+}
+</style>
