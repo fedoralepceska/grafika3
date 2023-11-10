ca75fa987e1971d1f443b8b0775c7ad3d1c66eb8
AdoCaice <63158828+AdoCaice@users.noreply.github.com>
2023-11-10 18:54:28 +0100

    Job Process and Spreadsheet component

---
+++ resources/js/Components/OrderSpreadsheet.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="spreadsheet">
+        <div class="flex justify-end">
+            <button @click="toggleEditMode" class="rounded text-black py-2 px-2 m-1"
+                    :class="{ 'bg-white': !editMode, 'green text-white': editMode }">
+                {{ editMode ? 'Exit Edit Mode \u2612' : 'Edit Mode \u2610' }}
+            </button>
+            <button @click="saveChanges()" v-if="editMode" class="blue rounded text-white py-2 px-5 m-1">Save Changes<v-icon class="mdi mdi-check"></v-icon></button>
+        </div>
+        <table >
+
+            <thead>
+            <tr>
+                <th>Job Line</th>
+                <th>Job Name</th>
+                <th>File Name</th>
+                <th >Quantity</th>
+                <th>Dims (cm)</th>
+                <th>Scheduled Ship Date</th>
+                <th>Shipping Address</th>
+                <th>Job Status</th>
+                <th>Unit Price</th>
+                <th>Job Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(job,index) in invoice.jobs" >
+                <td>#{{index+1}}</td>
+
+                <td v-if="editMode">
+                    <input type="text" class="text-black" v-model="job.editableName" />
+                </td>
+                <td v-else>{{job.name}}</td>
+
+                <td>{{ job.file}}</td>
+                <td v-if="editMode">
+                    <input type="text" class="text-black" v-model="job.editableQuantity" />
+                </td>
+                <td v-else>
+                    {{job.quantity}}
+                </td>
+                <td>{{job.height}}x{{job.width}}</td>
+                <td>{{ invoice?.end_date}}</td>
+                <td>{{job.shippingInfo}}</td>
+                <td>{{job.status}}</td>
+                <td v-if="editMode">
+                    <!--                                        treba da se dodade proverka dali e small ili large material i stoto da se editira
+                                                            job.materialsSmall.PricePerUnit ili job.materials.PricePerUnit-->
+                    <input type="text" class="text-black" v-model="job.materials.editablePricePerUnit" />
+                </td>
+                <td v-else>{{job.materials.pricePerUnit}}</td>
+
+                <td>{{job.materials.pricePerUnit*job.quantity}}.ден</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    props: {
+        job: Object,
+        invoice: Object,
+    },
+    data(){
+        return {
+            editMode: false,
+
+        }
+    },
+    methods: {
+        toggleEditMode() {
+            this.editMode = !this.editMode;
+            if (this.editMode) {
+                this.invoice.forEach((job) => {
+                    if (!job.editableQuantity || !job.editableName ||
+                        !job.materials.editablePricePerUnit) {
+                        job.editableQuantity = job.quantity;
+                        job.editableName = job.name;
+                        job.materials.editablePricePerUnit = job.materials.pricePerUnit
+                    }
+                });
+            }
+        },
+        async saveChanges() {
+            if (!this.editMode) {
+                // Exit edit mode
+                return;
+            }
+
+            // Handle the save changes action
+
+
+            // Reset the editable fields and exit edit mode
+
+            this.editMode = false;
+            window.location.reload();
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+table {
+    min-width: 191vh;
+    max-width: 191vh;
+    border-collapse: collapse;
+    border-spacing: 0;
+    margin-bottom: 5px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+table th, table td {
+    padding: 10px;
+    border: 1px solid #ddd;
+    text-align: center;
+}
+
+table th {
+    color: white;
+    border-top: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
+    background-color: $ultra-light-gray;
+
+}
+.blue{
+    background-color: $blue;
+}
+.green{
+    background-color: green;
+}
+</style>
---
+++ resources/js/Pages/Invoice/OrderJobDetails.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="pt-1 pl-1">
+    <div class="line flex gap-8 ">
+        <div v-for="action in actions(job.id)" :key="action">
+            <div class="flex">
+                <div class="action">
+                    <div class="flexed" :class="{
+                        'circle': true,
+                        'dark-gray': action.status === 'Not started yet',
+                        'green': action.status === 'Completed',
+                        'blue': action.status === 'In progress'
+                    }">
+                        <span v-if="action.status === 'Completed'">&#10003;</span>
+                    </div>
+                    <span>{{ $t(`actions.${action.name}`) }} </span>
+                </div>
+            </div>
+        </div>
+        <div class="completed line">
+            <div class="action">
+                <div class="flexed" :class="{
+                            'circle': true,
+                            'dark-gray': job.status === 'Not started yet',
+                            'green': job.status === 'Completed',
+                          }">
+                    <span v-if="job.status === 'Completed'">&#10003;</span>
+                </div>
+                <span>Completed</span>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    props: {
+        job: Object,
+        circle: Boolean,
+        blue: Boolean,
+        green: Boolean,
+        background: Boolean,
+    },
+    data(){
+        return {
+            newJobs: []
+        }
+    },
+    beforeMount() {
+        this.fetchJobs()
+    },
+    methods: {
+        async fetchJobs() {
+            try {
+                let response = await axios.get('/jobs');
+                this.newJobs = response.data;
+            } catch (error) {
+                console.error("Failed to fetch jobs:", error);
+            }
+        },
+        actions(id) {
+            const job = this.newJobs.find(job => job.id === id);
+            // Check if the job exists
+            if (job) {
+                const jobActions = job.actions;
+
+                if (jobActions && jobActions.length > 0) {
+                    return jobActions.map(action =>({
+                        name: action.name,
+                        status: action.status,
+                    }));
+                }
+            }
+
+            return false; // Return a default value if there are no actions for the job
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+.line {
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    // This will create the line
+    &:before {
+        content: '';
+        position: absolute;
+        top: 20px; // Adjust this value to match the vertical center of your circles
+        left: 70px;
+        right: 40px;
+        height: 2px; // Thickness of the line
+        background-color: $background-color; // Color of the line
+        z-index: 0; // Ensure it's behind the circles
+    }
+
+    .action {
+        // This will position the circle properly over the line
+        position: relative;
+        z-index: 1;
+    }
+
+    .circle {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 2px solid #D3D3D3; // Border color similar to the line if needed
+
+        // Checkmark or other content goes here
+        span {
+            color: #fff; // Adjust as necessary
+        }
+    }
+}
+.flexed{
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+    cursor: default;
+}
+.action, .completed{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.circle {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+}
+.blue {
+    background-color: $blue;
+}
+.green {
+    background-color: $green;
+}
+.dark-gray {
+    background-color: $dark-gray;
+}
+
+</style>
