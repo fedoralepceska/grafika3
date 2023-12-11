8b9f52aa26fbfd6017ba747fd6f7d96e288823b4
AdoCaice <63158828+AdoCaice@users.noreply.github.com>
2023-12-11 20:09:50 +0100

    Action Page frontend

--- resources/js/Pages/Production/ActionPage.vue
+++ resources/js/Pages/Production/ActionPage.vue
@@ -7,6 +7,81 @@
                 Invoices: {{invoices}} <br>
                 Action: {{id}} <br>
             </div>
+            <div v-for="(invoice,index) in invoices" class="main">
+                <div class="container flex gap-20 relative p-2">
+                <div class="bg-white text-black bold p-3 "><strong>{{invoice.invoice_title}}</strong></div>
+                <div class="info">
+                    <div>Order</div>
+                    <div class="bold">#{{ invoice.id }}</div>
+                </div>
+                <div class="info">
+                    <div>Customer</div>
+                    <div class="bold">{{invoice.client_id}}</div>
+<!--
+                    The Clients name should be fetched #TODO
+-->
+                </div>
+                <div class="info">
+                    <div>{{ $t('End Date') }}</div>
+                    <div class="bold">{{ invoice?.end_date }}</div>
+                </div>
+                <div class="info">
+                    <div>Created By</div>
+                    <div class="bold">{{ invoice.created_by }}</div>
+<!--
+                    The users name should be fetched #TODO
+-->
+                </div>
+                <div class="info">
+                    <div>Current Step</div>
+                    <div class="bold">{{ actionId }}</div>
+<!--
+                    The name of the action should be fetched/displayed #TODO
+-->
+                </div>
+                <div class="info">
+                    <div>Job ID</div>
+                    <div class="bold"></div>
+                </div>
+                <div class="btns">
+                    <div class="bt" @click="viewJobs(index)"><i class="fa-solid fa-bars"></i></div>
+                </div>
+                </div>
+                <div v-if="jobViewMode===index">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>LN</th>
+                                <th>Img</th>
+                                <th>Qty</th>
+                                <th>Copies</th>
+                                <th>Height</th>
+                                <th>Width</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+<!--
+                        Bi trebalo da e invoice.jobs za da gi dava samo za toj invoice #TODO
+-->
+                            <tr v-for="(job,index) in jobs">
+                                <td class="bg-white !text-black"><strong>#{{index+1}}</strong></td>
+                                <td class="flex">
+                                    <img :src="getImageUrl(job.id)" alt="Job Image" class="jobImg thumbnail"/>
+                                    {{job.file}}</td>
+                                <td>{{job.quantity}}</td>
+                                <td>{{job.copies}}</td>
+                                <td>{{job.height}}</td>
+                                <td>{{job.width}}</td>
+                                <td>
+                                    <button class="bg-white text-black p-2 rounded"><strong>Start job <i class="fa-regular fa-clock"></i>0min </strong></button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </MainLayout>
 </template>
@@ -28,7 +103,8 @@ export default {
         return {
             invoices: [],
             jobs: [],
-            id: null
+            id: null,
+            jobViewMode: null
         };
     },
     created() {
@@ -47,7 +123,59 @@ export default {
                 .catch(error => {
                     console.error('There was an error fetching the jobs:', error);
                 });
-        }
+        },
+        viewJobs(index) {
+            this.jobViewMode = this.jobViewMode === index ? null : index;
+        },
+        getImageUrl(id) {
+            return `/storage/uploads/${this.jobs.find(j => j.id === id).file}`
+        },
     }
 }
 </script>
+<style scoped lang="scss">
+.main{
+    background-color: $light-gray;
+}
+.info{
+    color: white;
+}
+.container{
+    background-color: $light-gray;
+    margin-bottom:10px
+}
+.btns{
+    position: absolute;
+    padding-right: 10px;
+    right: 0;
+}
+.bold{
+    font-weight: bolder;
+}
+.bt {
+    font-size: 30px;
+    cursor: pointer;
+    padding: 0;
+    color: white;
+}
+.jobImg {
+    width: 45px;
+    height: 45px;
+}
+table{
+    width: 100%;
+    background-color: $light-gray;
+    margin-bottom: 10px;
+}
+table, th, td{
+    border: 1px solid $ultra-light-gray;
+    color: white;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+}
+td{
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+</style>
