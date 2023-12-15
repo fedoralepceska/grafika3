91f42c782b4f890e7fa8e0a6bb2f0952f450993f
flepceska <f.lepcheska@heartforhealth.com>
2023-12-15 12:53:46 +0100

    fixed sync bug

--- resources/js/Pages/Invoice/OrderLines.vue
+++ resources/js/Pages/Invoice/OrderLines.vue
@@ -1,105 +1,77 @@
 <template>
-    <table class="border" v-if="$props.jobs?.length > 0">
-        <tbody>
-        <template v-if="updatedJobs.length === 0">
-            <tr v-for="(job, index) in $props.jobs" :key="index">
-                <!--FILE INFO BEFORE SYNCING-->
-                <div class=" text-white">
-                    <td class="text-black bg-gray-200 font-weight-black">#{{ index + 1 }}</td>
-                    <td> Name: <span class="bold">{{ job.file }}</span></td>
-                    <td>ID: <span class="bold">{{ job.id }}</span></td>
+    <div v-if="$props.jobs?.length > 0">
+        <table class="border">
+            <tbody>
+                <tr v-for="(job, index) in jobsToDisplay" :key="index">
+                    <!--ORDER INDEX, NAME AND ADDITIONAL INFO-->
+                    <div class="text-white">
+                        <td class="text-black bg-gray-200 font-weight-black "><span class="bold">#{{ index + 1 }}</span></td>
+                        <td> Name: <span class="bold">{{ job.file }}</span></td>
+                        <td>ID: <span class="bold">{{ job.id }}</span></td>
+                        <td>{{ $t('width') }}: <span class="bold">{{ job.width }}</span> </td>
+                        <td>{{ $t('height') }}: <span class="bold">{{ job.height }}</span></td>
+                        <td>{{$t('Quantity')}}: <span class="bold">{{ job.quantity }}</span></td>
+                        <td>{{$t('Copies')}}: <span class="bold">{{ job.copies }}</span></td>
+                    </div>
 
-                    <td>{{ $t('width') }}: <span class="bold">{{ job.width.toFixed(2) }}mm</span></td>
-                    <td>{{ $t('height') }}: <span class="bold">{{ job.height.toFixed(2) }}mm</span></td>
-                    <td>{{$t('Quantity')}}: <span class="bold">{{ job.quantity }}</span></td>
-                    <td>{{$t('Copies')}}: <span class="bold">{{ job.copies }}</span></td>
-                </div>
-
-
-                <div class="flex text-white">
-                    <td><img :src="getImageUrl(job.id)" alt="Job Image" class="jobImg thumbnail" /></td>
-
-                </div>
-
-                <div class="flex">
-                    <td class="flex items-center bg-gray-200 text-black">
-                        <img src="/images/shipping.png" class="w-10 h-10 pr-1" alt="Shipping">
-                        {{ $t('Shipping') }}: </td>
-                </div>
-
-            </tr>
-        </template>
-        <template v-else>
-            <tr v-for="(job, index) in updatedJobs" :key="index">
-                <!--ORDER INDEX, NAME AND ADDITIONAL INFO-->
-                <div class="text-white">
-                    <td class="text-black bg-gray-200 font-weight-black "><span class="bold">#{{ index + 1 }}</span></td>
-                    <td> Name: <span class="bold">{{ job.file }}</span></td>
-                    <td>ID: <span class="bold">{{ job.id }}</span></td>
-                    <td>{{ $t('width') }}: <span class="bold">{{ job.width }}</span> </td>
-                    <td>{{ $t('height') }}: <span class="bold">{{ job.height }}</span></td>
-                    <td>{{$t('Quantity')}}: <span class="bold">{{ job.quantity }}</span></td>
-                    <td>{{$t('Copies')}}: <span class="bold">{{ job.copies }}</span></td>
-                </div>
-
-                <!--FILE INFO-->
-                <div class="flex text-white">
-                    <td><img :src="getImageUrl(job.id)" alt="Job Image" class="jobImg thumbnail" /></td>
-                    <td>
-                        <div v-if="job.machinePrint">
-                            {{  $t('machineP') }} : <span class="bold"> {{$t(`machinePrint.${job.machinePrint}`) }}</span>
-                        </div>
-                    </td>
-                    <td>
-                        <div v-if="job.machineCut">
-                            {{  $t('machineC') }} : <span class="bold"> {{$t(`machineCut.${job.machineCut}`) }}</span>
-                        </div>
-                    </td>
-                    <td>
-                        <div v-if="job.materials">
-                            {{  $t('materialLargeFormat') }} : <span class="bold"> {{$t(`materials.${job.materials}`) }}</span>
-                        </div>
-                        <div v-if="job.materialsSmall">
-                            {{  $t('materialSmallFormat') }} : <span class="bold"> {{$t(`materialsSmall.${job.materialsSmall}`) }}</span>
-                        </div>
-                    </td>
-                </div>
-                <!-- ACTIONS -->
-                <div>
-                    <div class="pl-20 pr-14" v-if="actions(job.id)">
-                        <div class="jobInfo mt-3 mb-5 bg-gray-800">
-                            <div class="green p-1 pl-1 text-white bg-gray-700" @click="toggleActions" style="cursor: pointer">
-                                {{$t('ACTIONS')}}
-                                <button class="toggle-button" >&#9207;</button>
+                    <!--FILE INFO-->
+                    <div class="flex text-white">
+                        <td><img :src="getImageUrl(job.id)" alt="Job Image" class="jobImg thumbnail" /></td>
+                        <td>
+                            <div v-if="job.machinePrint">
+                                {{  $t('machineP') }} : <span class="bold"> {{$t(`machinePrint.${job.machinePrint}`) }}</span>
                             </div>
-                            <transition name="slide-fade">
-                                <div v-if="showActions" class="ultra-light-green text-white pl-1 pt-1 pb-1">
-                                    <div v-for="(action,index) in actions(job.id)" :key="action">
-                                        <span>{{index+1}}. <strong>{{ $t(`actions.${action}`) }}</strong></span>
-                                    </div>
+                        </td>
+                        <td>
+                            <div v-if="job.machineCut">
+                                {{  $t('machineC') }} : <span class="bold"> {{$t(`machineCut.${job.machineCut}`) }}</span>
+                            </div>
+                        </td>
+                        <td>
+                            <div v-if="job.materials">
+                                {{  $t('materialLargeFormat') }} : <span class="bold"> {{$t(`materials.${job.materials}`) }}</span>
+                            </div>
+                            <div v-if="job.materialsSmall">
+                                {{  $t('materialSmallFormat') }} : <span class="bold"> {{$t(`materialsSmall.${job.materialsSmall}`) }}</span>
+                            </div>
+                        </td>
+                    </div>
+                    <!-- ACTIONS -->
+                    <div>
+                        <div class="pl-20 pr-14" v-if="actions(job.id)">
+                            <div class="jobInfo mt-3 mb-5 bg-gray-800">
+                                <div class="green p-1 pl-1 text-white bg-gray-700" @click="toggleActions" style="cursor: pointer">
+                                    {{$t('ACTIONS')}}
+                                    <button class="toggle-button" >&#9207;</button>
                                 </div>
-                            </transition>
+                                <transition name="slide-fade">
+                                    <div v-if="showActions" class="ultra-light-green text-white pl-1 pt-1 pb-1">
+                                        <div v-for="(action,index) in actions(job.id)" :key="action">
+                                            <span>{{index+1}}. <strong>{{ $t(`actions.${action}`) }}</strong></span>
+                                        </div>
+                                    </div>
+                                </transition>
+                            </div>
+                        </div>
+                        <template v-else>
+                            <span></span>
+                        </template>
+                    </div>
+                    <!--SHIPPING INFO-->
+                    <div class="flex justify-between">
+                        <td class="flex items-center bg-gray-200 text-black">
+                            <img src="/images/shipping.png" class="w-10 h-10 pr-1" alt="Shipping">
+                            {{ $t('Shipping') }}:  <strong> {{ job.shippingInfo }}</strong></td>
+                        <div class="bg-white text-black bold">
+                            <div class="pt-4 pl-2 pr-2">
+                                {{$t('jobPrice')}}: <span class="bold">{{job?.totalPrice?.toFixed(2)}} ден.</span>
+                            </div>
                         </div>
                     </div>
-                    <template v-else>
-                        <span></span>
-                    </template>
-                </div>
-                <!--SHIPPING INFO-->
-                <div class="flex">
-                    <td class="flex items-center bg-gray-200 text-black">
-                        <img src="/images/shipping.png" class="w-10 h-10 pr-1" alt="Shipping">
-                        {{ $t('Shipping') }}:  <strong> {{ job.shippingInfo }}</strong></td>
-                    <div class="jobPriceInfo absolute right-0 bottom-0 bg-white text-black bold">
-                        <div class="p-2">
-                            {{$t('jobPrice')}}: <span class="bold">{{job.totalPrice.toFixed(2)}} ден.</span>
-                        </div>
-                    </div>
-                </div>
-            </tr>
-        </template>
-        </tbody>
-    </table>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
@@ -114,6 +86,12 @@ export default {
     data() {
         return {
             showActions: false
+        }
+    },
+
+    computed: {
+        jobsToDisplay() {
+            return this.updatedJobs.length < 1 ? this.$props.jobs : this.$props.updatedJobs;
         }
     },
 
