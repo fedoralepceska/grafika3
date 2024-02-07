785fc0756c3f07b041049761f744eae1c4b20fb5
AdoCaice <63158828+AdoCaice@users.noreply.github.com>
2024-02-07 20:22:39 +0100

    pdf blade changes

--- resources/views/invoices/pdf.blade.php
+++ resources/views/invoices/pdf.blade.php
@@ -3,13 +3,40 @@
 <head>
     <title>Invoice PDF</title>
     <style>
+        .order{
+            font-size: 28px;
+            font-weight: bolder;
+            padding-left: 5px;
+        }
+        .order1{
+            font-size: 18px;
+        }
+        .bolder{
+            font-weight: bolder;
+        }
+        .info {
+            /* no changes needed */
+        }
+
+        .left,
+        .right {
+            display: inline-block;
+            vertical-align: middle; /* align content vertically if needed */
+            width: 50%; /* optional: set equal widths */
+            padding: 5px;
+        }
         .invoice-info, .job-info {
             margin-bottom: 20px;
         }
         .divider {
-            height: 2px;
+            height: 1px;
             background-color: gray;
-            margin: 20px 0;
+        }
+        img{
+            display: flex;
+            justify-items: center;
+            justify-content: center;
+            align-items: center;
         }
         .page-break {
             page-break-after: always;
@@ -19,20 +46,30 @@
 <body>
 @foreach ($invoice->jobs as $job)
     <div class="invoice-info">
-        <h2>Invoice Number: {{ $invoice->id }}</h2>
-        <p>Start Date: {{ $invoice->start_date }}</p>
-        <p>End Date: {{ $invoice->end_date }}</p>
-        <p>Client: {{ $invoice->client->name }}</p>
-        <p>Contacts:</p>
-        @foreach ($invoice->client->contacts as $contact)
-            <span>{{ $contact->name }}</span>
-        @endforeach
-        <p>Comment: {{ $invoice->comment }}</p>
-        <p>Created By: {{ $invoice->user->name }}</p>
+        <div>
+        <div class="order1">Order: <span class="order">num.{{ $invoice->id }}/{{ date('Y') }}</span></div>
+        </div>
+        <div class="divider"></div>
+        <div class="info">
+            <table>
+                <tr>
+            <td class="left">
+                <div>Start Date: <span class="bolder">{{ $invoice->start_date }}</span></div>
+                <div>End Date: <span class="bolder">{{ $invoice->end_date }}</span></div>
+                <div>Created by: <span class="bolder">{{$invoice->created_by}}</span></div>
+            </td>
+            <td class="right">
+                <div>Client: <span class="bolder">{{ $invoice->client->name }}</span></div>
+                <div>Contact: <span class="bolder">{{$invoice->contact_id}}</span></div>
+                <div>Contact number: </div>
+            </td>
+                </tr>
+            </table>
+        </div>
+        <div class="divider"></div>
     </div>
-    <div class="divider"></div>
     <div class="job-info">
-        <h3>Job Name: {{ $job->file }}</h3>
+        <div>Job Name: {{ $job->file }}</div>
         <p>Width: {{ $job->width }}</p>
         <p>Height: {{ $job->height }}</p>
         <p>Material: {{ $job->small_material }}</p>
