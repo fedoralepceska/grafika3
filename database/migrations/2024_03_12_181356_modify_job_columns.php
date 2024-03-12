9d3c5fa3885e636448bba859949e7b3341216744
flepceska <f.lepcheska@heartforhealth.com>
2024-03-12 19:15:27 +0100

    modify company column

---
+++ database/migrations/2024_03_12_181356_modify_job_columns.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->float('width')->nullable()->change();
+            $table->float('height')->nullable()->change();
+            $table->string('file');
+            $table->string('originalFile');
+            $table->integer('estimatedTime')->nullable()->change();
+            $table->string('shippingInfo')->nullable()->change();
+            $table->integer('quantity')->nullable()->change();
+            $table->integer('copies')->nullable()->change();
+            $table->double('price')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->float('width')->nullable(false)->change();
+            $table->float('height')->nullable(false)->change();
+            $table->integer('estimatedTime')->nullable(false)->change();
+            $table->string('shippingInfo')->nullable(false)->change();
+            $table->integer('quantity')->nullable(false)->change();
+            $table->integer('copies')->nullable(false)->change();
+            $table->double('price')->nullable(false)->change();
+        });
+    }
+};
