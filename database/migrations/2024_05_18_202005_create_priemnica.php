daeaa49e4c9084adb2e3e1dce35c2cc6d027e41d
flepceska <f.lepcheska@heartforhealth.com>
2024-05-18 22:30:11 +0200

    added priemnica table

---
+++ database/migrations/2024_05_18_202005_create_priemnica.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('priemnica', function (Blueprint $table) {
+            $table->id();
+            $table->integer('warehouse')->nullable();
+            $table->unsignedBigInteger('client_id')->nullable();
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->unsignedBigInteger('article_id')->nullable();
+            $table->foreign('article_id')->references('id')->on('article')->onDelete('cascade');
+            $table->integer('quantity')->nullable();
+            $table->string('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('priemnica');
+    }
+};
