3a69b1c09c1b28a5dbfc73afd005b72e19c7fcd7
AdoCaice <63158828+AdoCaice@users.noreply.github.com>
2024-08-12 18:35:42 +0200

    Favicon update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'GrafikaPlus') }}</title>
-        <link rel="icon" href="{{ url('logo_blue.png') }}">
+        <link rel="icon" href="{{ url('favicon.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
