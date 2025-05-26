fdd2816a1e751f1c8850d8445d66db8b66a24614
AdoCaice <63158828+AdoCaice@users.noreply.github.com>
2025-05-26 10:35:06 +0200

    null handler on model function

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,6 @@ class User extends Authenticatable
     }
     public function hasRole($role)
     {
-        return $this->role->name === $role;
+        return $this->role && $this->role->name === $role;
     }
 }
